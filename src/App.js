6ba78db369ac450d748b97a48b32ed86cb8255b4
panyefan <pan_ye_fan@qq.com>
2018-11-10 17:41:46 +0800

    优化目录结构

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React,{Component} from 'react';
 import ReactDOM,{render} from 'react-dom';
 import {Router,Route,IndexRoute,hashHistory} from 'react-router';
-import './default.css';//加载公用样式
+import './styles/default.css';//加载公用样式
 import router from './router/router.js';//加载路由文件
 import './config/config';//加载配置参数
 
